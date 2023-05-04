364183ad6e43e72fdd1bea5c326185f79b8fad78
Fatih Yapıcı <fa.yapici@gmail.com>
2023-05-04 12:53:28 +0300

    fix get actor detail view model correction

--- MovieStoreWebApi/Applications/ActorOperations/Queries/GetActorDetail/GetActorDetailById.cs
+++ MovieStoreWebApi/Applications/ActorOperations/Queries/GetActorDetail/GetActorDetailById.cs
@@ -22,13 +22,13 @@ namespace MovieStoreWebApi.Applications.ActorOperations.Queries.GetActorDetail
             {
                 throw new InvalidOperationException("Aktör bulunamadi.");
             }
-            var directorViewModel = new GetActorDetailViewModel
+            var actorViewModel = new GetActorDetailViewModel
             {
                 Id = actor.Id,
                 Name = actor.Person.Name,
                 Surname = actor.Person.Surname
             };
-            return directorViewModel;
+            return actorViewModel;
         }
         public class GetActorDetailViewModel
         {
