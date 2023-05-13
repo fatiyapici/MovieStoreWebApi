6b313fd2df623a60751fcdbeb4a4c16b6c7dd78d
Fatih Yapıcı <fa.yapici@gmail.com>
2023-05-13 21:50:14 +0300

    refactor delete director command exception message fix

--- MovieStoreWebApi/Applications/DirectorOperations/Commands/DeleteDirector/DeleteDirectorCommand.cs
+++ MovieStoreWebApi/Applications/DirectorOperations/Commands/DeleteDirector/DeleteDirectorCommand.cs
@@ -4,7 +4,7 @@ namespace MovieStoreWebApi.Applications.DirectorOperations.Commands.DeleteDirect
 {
     public class DeleteDirectorCommand
     {
-        public int Id { get; set; }
+        public int DirectorId { get; set; }
         private readonly IMovieStoreDbContext _context;
         public const string ExceptionMessage = "Yönetmen bulunamadi.";
 
@@ -14,10 +14,10 @@ namespace MovieStoreWebApi.Applications.DirectorOperations.Commands.DeleteDirect
         }
         public void Handle()
         {
-            var director = _context.Directors.Find(Id);
+            var director = _context.Directors.Find(DirectorId);
             if (director is null)
             {
-                throw new InvalidOperationException("Yönetmen bulunamadi.");
+                throw new InvalidOperationException(ExceptionMessage);
             }
             _context.Directors.Remove(director);
             _context.SaveChanges();
